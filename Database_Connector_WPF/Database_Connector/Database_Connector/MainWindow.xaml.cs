1dd2863797b188f2ef29aba28e8d89344d4164e6
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-30 08:57:11 +0100

    Update

--- Database_Connector_WPF/Database_Connector/Database_Connector/MainWindow.xaml.cs
+++ Database_Connector_WPF/Database_Connector/Database_Connector/MainWindow.xaml.cs
@@ -1,25 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
+using System.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Database_Connector {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
+
+        MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+
+        public MainWindow() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            try {
+
+                /* conn.Open();
+                MySqlCommand cdm = new MySqlCommand("SELECT * FROM testtable", conn);
+                MySqlDataReader myReader;
+                myReader = cdm.ExecuteReader();
+                while (myReader.Read()) { MessageBox.Show(myReader.GetString(0)); }
+                myReader.Close(); */
+
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM testtable", conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "LoadDataBinding");
+                dgPruebas.DataContext = ds;
+            }
+            catch (MySqlException ex) { MessageBox.Show(ex.ToString()); }
+            finally { conn.Close(); }
         }
     }
 }
