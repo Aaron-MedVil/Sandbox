33dc8367b8ff475c8b54bb9a5b8e651da11a1045
xaxe96 <a.medina.v.96@hotmail.com>
2020-11-30 09:14:03 +0100

    Update

--- Database_Connector_WPF/Database_Connector/Database_Connector/TestTableData.cs
+++ Database_Connector_WPF/Database_Connector/Database_Connector/TestTableData.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Database_Connector {
-    class Class1 {
+
+    class TestTableData {
+
+        private int id;
+        private string nombre;
+
+        /// <summary>Constructor</summary>
+        /// <param name="id">Identificador del registro</param>
+        /// <param name="nombre">Nombre del registro</param>
+        public TestTableData(int id, string nombre) {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        /// <summary>Getter/Setter id</summary>
+        public int Id { get => id; set => id = value; }
+
+        /// <summary>Getter/Setter nombre</summary>
+        public string Nombre { get => nombre; set => nombre = value; }
     }
 }
