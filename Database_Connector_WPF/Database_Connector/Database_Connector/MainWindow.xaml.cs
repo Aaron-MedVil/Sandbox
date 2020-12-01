d54dc1c07671f2ad9de1aff31fc2d68fb6d2d23c
xaxe96 <a.medina.v.96@hotmail.com>
2020-12-01 10:38:02 +0100

    Update

--- Database_Connector_WPF/Database_Connector/Database_Connector/MainWindow.xaml.cs
+++ Database_Connector_WPF/Database_Connector/Database_Connector/MainWindow.xaml.cs
@@ -9,20 +9,14 @@ namespace Database_Connector {
     public partial class MainWindow : Window {
 
         private MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
-        private MySqlConnection conn2 = new MySqlConnection(ConfigurationManager.ConnectionStrings["connectionString2"].ConnectionString);
-
         private MySqlDataReader myReader = null;
 
-        private List<Test> testList = new List<Test>();
-
+        /// <summary>Funcion que carga los componentes</summary>
         public MainWindow() => InitializeComponent();
 
-        /// <summary>Carga los datos de la base de datos en el DataGrid</summary>
-        /// <param name="sender">Objeto que realiza la accion</param>
-        /// <param name="e">Argumentos de la accion</param>
-        private void Button_Click(object sender, RoutedEventArgs e) {
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
 
-            try {
+            /*try {
 
                 conn.Open();
                 MySqlCommand cmd = new MySqlCommand("SELECT * FROM testtable", conn);
@@ -32,38 +26,11 @@ namespace Database_Connector {
                 dgPruebas.DataContext = ds;
             }
             catch (MySqlException ex) { MessageBox.Show(ex.ToString()); }
-            finally { conn.Close(); }
+            finally { conn.Close(); }*/
         }
 
-        /// <summary>Carga los datos de la base de datos en una lista</summary>
-        /// <param name="sender">Objeto que realiza la accion</param>
-        /// <param name="e">Argumentos de la accion</param>
-        private void Button_Click_1(object sender, RoutedEventArgs e) {
+        private void btnReloadData_Click(object sender, RoutedEventArgs e) {
 
-            try {
-
-                conn2.Open();
-                MySqlCommand cdm2 = new MySqlCommand("SELECT Host, Db, User FROM db", conn2);
-                myReader = cdm2.ExecuteReader();
-
-                while (myReader.Read()) {
-
-                    Test t = new Test();
-                    t.Host = myReader["Host"].ToString();
-                    t.Db = myReader["Db"].ToString();
-                    t.User = myReader["User"].ToString();
-
-                    testList.Add(t);
-                }
-
-                myReader.Close();
-
-                foreach (var item in testList) {
-                    MessageBox.Show("User: " + item.User.ToString() + "\nHost: " + item.Host.ToString() + " \nDb: " + item.Db.ToString());
-                }
-            }
-            catch (MySqlException ex) { MessageBox.Show(ex.ToString()); }
-            finally { conn2.Close(); }
         }
     }
 }
